e7a51c6b36d74a88da8c39ce44c5b8e293997111
Mauricio Olguin <mauolguisanchez@gmail.com>
2021-10-05 09:14:36 -0500

    Programa organizado y mejorado

--- funcs.py
+++ funcs.py
@@ -1,23 +1,111 @@
+from turtle import Turtle,Screen, colormode
+from PIL import Image
+from statistics import mode
+from time import time
 
-def rectangle_pixel(lista,indice):
-    try:
-        value_i = lista[indice]
-        new_indice = indice + 1
-        new_list = []
-        counter = 1
-        while value_i == lista[new_indice] and new_indice < len(lista)-1:
-            # if new_indice < len(lista)-1:
-            counter+=1
-            new_indice+= 1
-            # else:
-            #     cola = new_list.append(value_i,counter)
-            #     return cola
-        new_list.append([value_i,counter])
-    except IndexError:
-        return None
-    return new_list
+class ImageToTurtle:
 
-xd = [1,2,2,2,3,3,3]
-print(rectangle_pixel(xd,0))
-print(rectangle_pixel(xd,1))
-print(rectangle_pixel(xd,7))
+    colormode(255)
+
+    def __init__(self,nombre_imagen,zoom):
+        self.nombre_imagen = nombre_imagen
+        self.zoom = zoom
+    
+
+    def point_maker(self,object,color):
+        object.pendown()
+        object.color(color)
+        object.dot(self.zoom)
+        object.penup()
+
+
+    def redimention_image(self):
+        im = Image.open(self.nombre_imagen) 
+        pix = im.load()
+        size = im.size
+        print(size)
+        if size[0] != size[1] or size[0] > 100:
+            im = im.resize((100,100))
+            pix = im.load()
+            size = im.size
+        print(size)
+        return pix,size
+
+
+    def cords_colors(self,size,pix):
+        cords_colors = []
+        for x in range(0,size[0]):
+            for y in range(0,size[1]):
+                cords_colors.append(pix[y,x])
+        frecuent_color = mode(cords_colors)
+        return cords_colors,frecuent_color
+
+
+    def similar_color(self,frecuent_color):
+        similar_color = []
+        for sc in range(1,101):
+            tone_r = [frecuent_color[0]+sc,frecuent_color[0]-sc]
+            for rr in tone_r:
+                for sc2 in range(1,101):
+                    tone_g = [frecuent_color[1]+sc,frecuent_color[1]-sc2]
+                    for gg in tone_g:
+                        for sc3 in range(1,101):
+                            tone_b = [frecuent_color[2]+sc,frecuent_color[2]-sc3]
+                            for bb in tone_b:
+                                similar_color.append((rr,gg,bb))
+        similar_color = list(set(similar_color))
+        return similar_color
+
+
+    def image_maker(self):
+        inicio = time()
+        pix,size = self.redimention_image()
+        cords_colors,frecuent_color = self.cords_colors(size,pix)
+        similar_color = self.similar_color(frecuent_color)
+        turtle = Turtle(visible=False)
+        turtle.shape('turtle')
+        turtle.penup()
+        turtle.speed(10)
+        screen = Screen()
+        screen.tracer(False)
+        screen.bgcolor(frecuent_color) 
+        screen.title("The painter turtle")
+        # screen.setup(width=size[0]*(zoom*3),height=size[1]*(zoom*3))
+        # turtle.goto(-(size[0]*zoom),size[1]*zoom)
+        turtle.goto(-(size[0]/2)*(self.zoom),(size[1]/2)*(self.zoom))
+        pixel_to_draw = 0
+        tiempos_renglones = []
+        pixeles_hechos_per_renglon = []
+        for i in range(0,size[0]):
+            pixel_hecho = 0
+            tiempo_renglon_inicio = time()
+            for j in range(0,size[1]):
+                if cords_colors[pixel_to_draw] in similar_color or cords_colors[pixel_to_draw] == frecuent_color:
+                    turtle.forward(self.zoom)
+                    pixel_to_draw += 1
+                else:
+                    self.point_maker(turtle,cords_colors[pixel_to_draw])
+                    turtle.forward(self.zoom)
+                    pixel_to_draw += 1
+                    pixel_hecho += 1
+            turtle.backward((size[0]*self.zoom))
+            turtle.right(90)
+            turtle.forward(self.zoom)
+            turtle.left(90)
+            tiempo_renglon_fin = time()
+            tiempo_renglon = round(tiempo_renglon_fin-tiempo_renglon_inicio,2)
+            tiempos_renglones.append(tiempo_renglon)
+            pixeles_hechos_per_renglon.append(pixel_hecho)
+            print(tiempo_renglon)
+            # if len(tiempos_renglones)%10==0:
+            #     print(f'Numero de renglones renglones hechos: {len(tiempos_renglones)}')
+            #     promedio = (sum(tiempos_renglones)/60)*size[0]
+            #     print(f"Tiempor aproximado para acabar: {promedio} minutos")
+            screen.update()
+
+        screen.tracer(True)
+        turtle.hideturtle() #Para ocultar a la torutga
+
+        fin = time()
+        print(f'Tiempo empleado para dibujar: {round((fin-inicio)/60,2)} minutos')
+        screen.exitonclick()
